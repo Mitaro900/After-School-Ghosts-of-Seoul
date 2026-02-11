8cdcbc65b79798c6025b0ae8d3c33e9ba22b0e62
Mitaro900 <jhkim9622@gmail.com>
2026-02-11 16:13:11 +0900

    Update NpcChat.cs
    맞춤법 수정

--- Assets/Scripts/Common/NPC/NpcChat.cs
+++ Assets/Scripts/Common/NPC/NpcChat.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 
 public class NpcChat : BaseChat
 {
-    private void sandNpcChat(string npcText)
+    private void SendNpcChat(string npcText)
     {
         string text = npcText;  // 입력값 저장
         Send(text); // 채팅 발송 (속도, 얼굴은 BaseChat에서 바로 넣는 중)
