acaa9656d34f97ed43af573aa9c998956397b467
박민규 <pumkin8789@gmail.com>
2026-02-24 17:26:49 +0900

    학교 스프라이트 추가, 플레이어 이동, 지정구역에만 카메라 이동 기능 추가
    - 같은 자리에서 텔포 시도시 2번클릭해야 다음 장소로 움직이는 버그 있음

--- Assets/Scripts/Common/CameraManager.cs
+++ Assets/Scripts/Common/CameraManager.cs
@@ -5,16 +5,63 @@ public class CameraManager : Singleton<CameraManager>
     [SerializeField] private Camera cam;
     private Transform Target;
 
+    private float minX, maxX, minY, maxY;
+    private bool hasBounds = false;
+
+    private float camHeight;
+    private float camWidth;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        camHeight = cam.orthographicSize;
+        camWidth = camHeight * cam.aspect;
+    }
 
     private void LateUpdate()
     {
-        // 타겟쪽으로 카메라가 따라감
-        if (Target != null)
-            cam.transform.position = new Vector3(Target.position.x, Target.position.y, cam.transform.position.z);
+        if (Target == null) return;
+
+        float targetX = Target.position.x;
+        float targetY = Target.position.y;
+
+        if (hasBounds)
+        {
+            targetX = Mathf.Clamp(targetX, minX + camWidth, maxX - camWidth);
+            targetY = Mathf.Clamp(targetY, minY + camHeight, maxY - camHeight);
+        }
+
+        cam.transform.position = new Vector3(targetX, targetY, cam.transform.position.z);
     }
 
     public void FollowCamera(Transform target)
     {
         Target = target;
     }
+
+    // 구역 변경 시 호출
+    public void SetBounds(BoxCollider2D bounds)
+    {
+        Bounds b = bounds.bounds;
+
+        minX = b.min.x;
+        maxX = b.max.x;
+        minY = b.min.y;
+        maxY = b.max.y;
+
+        hasBounds = true;
+    }
+
+    public void ForceMoveToTarget()
+    {
+        if (Target == null) return;
+
+        cam.transform.position = new Vector3(
+            Target.position.x,
+            Target.position.y,
+            cam.transform.position.z
+        );
+    }
+
 }
